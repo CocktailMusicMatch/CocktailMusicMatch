6b6c4642f9aaa0822e81924dc43ac66f121b7338
GriffinNB <griffinnovetsky@gmail.com>
2022-10-26 19:57:49 -0400

    added initial modal for generate button

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -81,3 +81,15 @@ function drinkApi(selection){
 
 }
 
+// Modal
+
+window.openModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'block'
+    document.getElementsByTagName('body')[0].classList.add('overflow-y-hidden')
+  }
+  
+  window.closeModal = function(modalId) {
+    document.getElementById(modalId).style.display = 'none'
+    document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+  }
+
